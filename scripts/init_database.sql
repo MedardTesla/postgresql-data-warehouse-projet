227be0ba286f48f5c7c97e4186fcc7ee9088553f
Medard MINLEKIB <87976610+MedardTesla@users.noreply.github.com>
2025-03-02 23:02:39 +0000

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,12 @@
 
+-- Create Database 'DataWarehouse'
+-- Database was created with pgadmin
+create database DataWarehouse;
+-- select * from pg_database where datname='datawarehouse';
+-- connect to database datawarehouse before executing the following query
+-- Create  Schema
+create schema bronze;
+
+create schema silver;
+
+create schema gold;

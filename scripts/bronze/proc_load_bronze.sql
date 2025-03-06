b96fd4b409b691a96a4b5cd2e5a11283b13548a9
Medard MINLEKIB <87976610+MedardTesla@users.noreply.github.com>
2025-03-06 23:14:26 +0000

    Update proc_load_bronze.sql

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -1,76 +1,140 @@
 
-CREATE OR replace PROCEDURE bronze.load_bronze
-  () LANGUAGE plpgsql
-AS
-  $$
-  DECLARE
-    start_time timestamp;
-    end_time timestamp;
-  BEGIN
-    RAISE notice '=========================================';
-    RAISE notice 'Loading Bronze Layer';
-    RAISE notice '=========================================';
-    RAISE notice '------------------------------------------';
-    RAISE notice 'Loading CRM table';
-    RAISE notice '------------------------------------------';
-    RAISE notice 'Truncating table: bronze.crm_cust_info';
-    TRUNCATE TABLE bronze.crm_cust_info;
-    
-    RAISE notice '>> Inserting table: bronze.crm_cust_info';
-    start_time = now();
-    copy bronze.crm_cust_info FROM 'C:\sql\dwh_project\source_crm\cust_info.csv' WITH ( format csv, header TRUE, delimiter ',', NULL '', encoding 'UTF8' );
-    end_time = now();
-    RAISE notice 'Load duration: % s', end_time - start_time;
-    RAISE notice '';
-    RAISE notice 'Truncating table: bronze.crm_prd_info';
-    TRUNCATE TABLE bronze.crm_prd_info ;
-    
-    RAISE notice '>>Inserting table: bronze.crm_prd_info';
-    start_time = now();
-    copy bronze.crm_prd_info FROM 'C:\sql\dwh_project\source_crm\prd_info.csv' WITH ( format csv, header TRUE, delimiter ',', NULL '', encoding 'UTF8' );
-    end_time = now();
-    RAISE notice 'Load duration: % s', end_time - start_time;
-    RAISE notice '';
-    RAISE notice 'Truncating table: bronze.crm_sales_details';
-    TRUNCATE TABLE bronze.crm_sales_details ;
-    
-    RAISE notice '>>Inserting table: bronze.crm_sales_details';
-    start_time = now();
-    copy bronze.crm_sales_details FROM 'C:\sql\dwh_project\source_crm\sales_details.csv' WITH ( format csv, header TRUE, delimiter ',', NULL '', encoding 'UTF8' );
-    end_time = now();
-    RAISE notice 'Load duration: % s', end_time - start_time;
-    RAISE notice '------------------------------------------';
-    RAISE notice 'Loading ERP table';
-    RAISE notice '------------------------------------------';
-    RAISE notice '';
-    RAISE notice 'Truncating table: bronze.erp_cust_az12';
-    TRUNCATE TABLE bronze.erp_cust_az12 ;
-    
-    RAISE notice '>>Inserting table: bronze.erp_cust_az12';
-    start_time = now();
-    copy bronze.erp_cust_az12 FROM 'C:\sql\dwh_project\source_erp\CUST_AZ12.csv' WITH ( format csv, header TRUE, delimiter ',', NULL '', encoding 'UTF8' );
-    end_time = now();
-    RAISE notice 'Load duration: % s', end_time - start_time;
-    RAISE notice '';
-    RAISE notice 'Truncating table: bronze.erp_loc_a101';
-    TRUNCATE TABLE bronze.erp_loc_a101 ;
-    
-    RAISE notice '>>Inserting table: bronze.erp_loc_a101';
-    start_time = now();
-    copy bronze.erp_loc_a101 FROM 'C:\sql\dwh_project\source_erp\LOC_A101.csv' WITH ( format csv, header TRUE, delimiter ',', NULL '', encoding 'UTF8' );
-    end_time = now();
-    RAISE notice 'Load duration: % s', end_time - start_time;
-    RAISE notice '';
-    RAISE notice 'Truncating table: bronze.erp_px_cat_g1v2';
-    TRUNCATE TABLE bronze.erp_px_cat_g1v2 ;
-    
-    RAISE notice '>>Inserting table: bronze.erp_px_cat_g1v2';
-    start_time = now();
-    copy bronze.erp_px_cat_g1v2 FROM 'C:\sql\dwh_project\source_erp\PX_CAT_G1V2.csv' WITH ( format csv, header TRUE, delimiter ',', NULL '', encoding 'UTF8' );
-    end_time = now();
-    RAISE notice 'Load duration: % s', end_time - start_time;
-  EXCEPTION
-  WHEN OTHERS THEN
-    RAISE notice 'Une erreur inattendue s''est produite : %', SQLERRM;
-  END;
-  $$;
+
+create or replace procedure bronze.load_bronze()
+language plpgsql
+as $$
+declare 
+	start_time timestamp;
+	end_time timestamp;
+begin 
+	
+	raise notice '=========================================';
+	raise notice 'Loading Bronze Layer';
+	raise notice '=========================================';
+	
+	raise notice '------------------------------------------';
+	raise notice 'Loading CRM table';
+	raise notice '------------------------------------------';
+
+	raise notice 'Truncating table: bronze.crm_cust_info';
+	truncate table bronze.crm_cust_info;
+	raise notice '>> Inserting table: bronze.crm_cust_info';
+	start_time = now();
+	copy bronze.crm_cust_info
+	from 'C:\sql\dwh_project\source_crm\cust_info.csv'
+	with (
+		format csv,
+		header true,
+		delimiter ',',
+		null '',
+		encoding 'UTF8'
+	);
+	end_time = now();
+	raise notice 'Load duration: % s', end_time - start_time;
+	
+	
+	
+	raise notice '';
+	raise notice 'Truncating table: bronze.crm_prd_info';
+	truncate table bronze.crm_prd_info ;
+	raise notice '>>Inserting table: bronze.crm_prd_info';
+
+	start_time = now();
+	copy bronze.crm_prd_info
+	from 'C:\sql\dwh_project\source_crm\prd_info.csv'
+	with (
+		format csv,
+		header true,
+		delimiter ',',
+		null '',
+		encoding 'UTF8'
+	);
+
+	end_time = now();
+	raise notice 'Load duration: % s', end_time - start_time;
+
+	raise notice '';
+	raise notice 'Truncating table: bronze.crm_sales_details';
+	truncate table bronze.crm_sales_details ;
+	raise notice '>>Inserting table: bronze.crm_sales_details';
+	
+	start_time = now();
+	copy bronze.crm_sales_details
+	from 'C:\sql\dwh_project\source_crm\sales_details.csv'
+	with (
+		format csv,
+		header true,
+		delimiter ',',
+		null '',
+		encoding 'UTF8'
+	);
+	end_time = now();
+	raise notice 'Load duration: % s', end_time - start_time;
+
+	raise notice '------------------------------------------';
+	raise notice 'Loading ERP table';
+	raise notice '------------------------------------------';
+	
+
+	
+
+	
+	raise notice '';
+	raise notice 'Truncating table: bronze.erp_cust_az12';
+	truncate table bronze.erp_cust_az12 ;
+	raise notice '>>Inserting table: bronze.erp_cust_az12';
+	start_time = now();
+	copy bronze.erp_cust_az12
+	from 'C:\sql\dwh_project\source_erp\CUST_AZ12.csv'
+	with (
+		format csv,
+		header true,
+		delimiter ',',
+		null '',
+		encoding 'UTF8'
+	);
+	end_time = now();
+	raise notice 'Load duration: % s', end_time - start_time;
+	
+	raise notice '';
+	raise notice 'Truncating table: bronze.erp_loc_a101';
+	truncate table bronze.erp_loc_a101  ;
+	raise notice '>>Inserting table: bronze.erp_loc_a101';
+
+	start_time = now();
+	copy bronze.erp_loc_a101
+	from 'C:\sql\dwh_project\source_erp\LOC_A101.csv'
+	with (
+		format csv,
+		header true,
+		delimiter ',',
+		null '',
+		encoding 'UTF8'
+	);
+	end_time = now();
+	raise notice 'Load duration: % s', end_time - start_time;
+	
+	raise notice '';
+	raise notice 'Truncating table: bronze.erp_px_cat_g1v2';
+	truncate table bronze.erp_px_cat_g1v2  ;
+	raise notice '>>Inserting table: bronze.erp_px_cat_g1v2';
+
+	start_time = now();
+	copy bronze.erp_px_cat_g1v2
+	from 'C:\sql\dwh_project\source_erp\PX_CAT_G1V2.csv'
+	with (
+		format csv,
+		header true,
+		delimiter ',',
+		null '',
+		encoding 'UTF8'
+	);
+	end_time = now();
+	raise notice 'Load duration: % s', end_time - start_time;
+exception 
+    when  others then 
+        raise notice 'Une erreur inattendue s''est produite : %', SQLERRM;
+end;
+
+$$;
+
